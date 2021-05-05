bb898c302e1d6950a12bdabaa069646d9ad270c4
Shananhsu <Shananhsu@me.com>
2021-05-06 04:35:17 +0800

    輪播圖尺寸配色第一版

--- client/src/components/HomePageCursor.jsx
+++ client/src/components/HomePageCursor.jsx
@@ -9,19 +9,19 @@ class HomePageCursor extends Component {
             // 自動播放 無限循環 設定4秒 可滑動 下方幻燈片開關 右上角圖片計數開關
             <Carousel autoPlay infiniteLoop interval="4000" swipeable showThumbs={false} showStatus={false}>
                 <div>
-                    <img src="images/cursor/index-00.jpg" alt ="imgErr"/>
+                    <img src="images/cursor/Carousel_01.png" alt ="imgErr"/>
                 </div>
                 <div>
-                    <img src="images/cursor/index-02.jpg" alt ="imgErr"/>
+                    <img src="images/cursor/Carousel_01.png" alt ="imgErr"/>
                 </div>
                 <div>
-                    <img src="images/cursor/index-03.jpg" alt ="imgErr"/>
+                    <img src="images/cursor/Carousel_01.png" alt ="imgErr"/>
                 </div>
                 <div>
-                    <img src="images/cursor/index-05.jpg" alt ="imgErr"/>
+                    <img src="images/cursor/Carousel_01.png" alt ="imgErr"/>
                 </div>
                 <div>
-                    <img src="images/cursor/index-06.jpg" alt ="imgErr"/>
+                    <img src="images/cursor/Carousel_01.png" alt ="imgErr"/>
                 </div>
             </Carousel>
         );
