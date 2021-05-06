41aad924bccd1255fc9254bb1cad9d1a38f5c6fd
Silarce <silarceanoux@gmail.com>
2021-05-06 14:14:06 +0800

    修改遊戲大廳排版

--- client/src/components/Maingamelink.jsx
+++ client/src/components/Maingamelink.jsx
@@ -11,38 +11,37 @@ class Maingamelink extends Component {
                     <div className="row">
 
                         <div id="sf-lobby-00001">
-                            <a className="col-sm-3 col-xs-6 game" href="./games/blackjack/index.html"
+                            <a className="game" href="./games/blackjack/index.html"
                                 target="game" data-category="" data-sub_category="">
                                 <img src="/images/gamelink/blackjack.jpg" />21點
-
                             </a>
 
-                            <a className="col-sm-3 col-xs-6 game" href="./games/niuniu/public/index.html"
+                            <a className="game" href="./games/niuniu/public/index.html"
                                 target="game" data-category="" data-sub_category="">
-                                <img src="/images/gamelink/niuniu.jpg" style={{ "max-height": "50%" }} />妞妞
+                                <img src="/images/gamelink/niuniu.jpg"  />妞妞
                             </a>
 
-                            <a className="col-sm-3 col-xs-6 game" href="./games/dragonshoot/index.html"
+                            <a className="game" href="./games/dragonshoot/index.html"
                                 target="game" data-category="" data-sub_category="">
                                 <img src="/images/gamelink/dragondoor.png" />射龍門
                             </a>
 
-                            <a className="col-sm-3 col-xs-6 game" href="./games/fish/fishShooter.html"
+                            <a className="game" href="./games/fish/fishShooter.html"
                                 target="game" data-category="" data-sub_category="">
                                 <img src="/images/gamelink/fish.jpg" />捕魚遊戲
                             </a>
 
-                            <a className="col-sm-3 col-xs-6 game" href="./games/tiger/index.html"
+                            <a className="game" href="./games/tiger/index.html"
                                 target="game" data-category="" data-sub_category="">
                                 <img src="/images/gamelink/tiger.jpg" />老虎機
                             </a>
 
-                            <a className="col-sm-3 col-xs-6 game" href="./games/billiard/index.html"
+                            <a className="game" href="./games/billiard/index.html"
                                 target="game" data-category="" data-sub_category="">
                                 <img src="/images/gamelink/tableball.jpg" />桌球遊戲
                             </a>
 
-                            <a className="col-sm-3 col-xs-6 game" href="./games/baccarat/index.html"
+                            <a className="game" href="./games/baccarat/index.html"
                                 target="game" data-category="" data-sub_category="">
                                 <img src="/images/gamelink/baccarat.jpg" />百家樂
                             </a>
